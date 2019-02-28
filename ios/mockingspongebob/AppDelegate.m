6b128cb47ac9fbf96495dcf0ca5876cfc054dca2
Brian Lindner <brian.lindner@retailbusinessservices.com>
2019-02-28 16:08:21 -0500

    Renamed

--- ios/mockingspongebob/AppDelegate.m
+++ ios/mockingspongebob/AppDelegate.m
@@ -19,7 +19,7 @@
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"reactnativetest"
+                                                      moduleName:@"mockingspongebob"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [UIColor blackColor];
